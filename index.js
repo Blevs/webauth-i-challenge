faa1110c4ac3ac8575cebb056899a6f8e3a7b2f4
Henry Blevins <heblevi@gmail.com>
2019-05-06 18:12:27 -0400

    Add login route that returns session cookie

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const session = require('express-session');
 
 const server = express();
 server.use(express.json());
+
+// configure express-session middleware
+server.use(
+  session({
+    name: 'webauthi', // default is connect.sid
+    secret: 'thisshouldnotbehere',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      // secure: true, // only set cookies over https. Server will not send back a cookie over http.
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const users = require('./users/routes.js');
 server.use('/api/', users);
--- users/routes.js
+++ users/routes.js
@@ -17,6 +17,23 @@ router.post('/register', (req, res) => {
   }
 });
 
+router.post('/login', (req, res) => {
+  const {username, password} = req.body;
+  db('users')
+    .select()
+    .where({username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.userID = user.id;
+        res.status(200).json({message: "Logged in"});
+      } else {
+        res.status(401).json({message: "Bad credentials"});
+      }
+    })
+    .catch(err => console.log(err) || res.status(500).json({message: "Error authenticating"}));
+});
+
 router.get('/users', (req, res) => {
   db('users')
     .then(users => res.status(200).json(users))
